b1f1de0369f2bcc2d000c41015f9bb5c19b90a28
Ivy930626 <chiivy0626@gmail.com>
2024-05-22 16:05:17 +0800

    v3_謝棨亘

--- app/src/main/java/tw/edu/pu/csim/example/s1114701/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/example/s1114701/MainActivity.kt
@@ -4,12 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -24,12 +31,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import tw.edu.pu.csim.example.s1114701.ui.theme.S1114701Theme
 
@@ -44,6 +54,88 @@ class MainActivity : ComponentActivity() {
                     Main()
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun FirstScreen(navController: NavHostController) {
+    var appear by remember { mutableStateOf(true) }
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.White),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start
+
+    ) {
+        Text(
+            text = "瑪利亞基金會服務總覽" ,
+            color = Color.Blue
+        )
+        AnimatedVisibility(
+            visible = appear,
+            enter = fadeIn(
+                initialAlpha = 0.1f,
+                animationSpec = tween(durationMillis = 3000)
+            ),
+            exit = fadeOut(
+                animationSpec = tween(durationMillis = 3000)
+            )
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.service),
+                contentDescription = "service",
+            )
+        }
+        Button(
+            onClick = {
+                appear = false
+                navController.navigate("JumpSecond")
+            }
+        ) {
+            Text(text = "作者：資管二Ａ謝棨亘")
+        }
+    }
+}
+
+@Composable
+fun SecondScreen(navController: NavHostController) {
+    var appear by remember { mutableStateOf(true) }
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.White),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "關於App作者" ,
+            color = Color.Blue
+        )
+        AnimatedVisibility(
+            visible = appear,
+            enter = fadeIn(
+                initialAlpha = 0.1f,
+                animationSpec = tween(durationMillis = 3000)
+            ),
+            exit = fadeOut(
+                animationSpec = tween(durationMillis = 3000)
+            )
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.photo),
+                contentDescription = "service",
+                modifier = Modifier.size(400.dp)
+            )
+        }
+        Button(
+            onClick = {
+                appear = false
+                navController.navigate("JumpFirst")
+            }
+        ) {
+            Text(text = "服務總覽")
         }
     }
 }
@@ -66,7 +158,6 @@ fun Main() {
                         Image(
                             painter = painterResource(id = R.drawable.maria),
                             contentDescription = "button icon",
-                            modifier = Modifier.size(120.dp)
                         )
                     },
                     actions = {
@@ -96,10 +187,17 @@ fun Main() {
                         }
                     }
                 )
-                Text(text = "簡介",color = Color.Blue)
+                //Text(text = "簡介",color = Color.Blue)
+
+                NavHost(navController = navController, startDestination = "JumpFirst") {
+                    composable("JumpFirst") {
+                        FirstScreen(navController = navController)
+                    }
+                    composable("JumpSecond") {
+                        SecondScreen(navController = navController)
+                    }
+                }
             }
         }
     }
 }
-
-
