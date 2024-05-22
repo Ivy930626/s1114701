7cc79b09359166d8ec70eeb713fddd766e2fa084
Ivy930626 <chiivy0626@gmail.com>
2024-05-22 16:35:50 +0800

    v4_謝棨亘

--- app/src/main/java/tw/edu/pu/csim/example/s1114701/SecondActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/example/s1114701/SecondActivity.kt
@@ -1,14 +1,18 @@
 package tw.edu.pu.csim.example.s1114701
 
 import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.magnifier
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Button
@@ -28,10 +32,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -52,6 +58,62 @@ class SecondActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+@Composable
+fun FirstScreen1(navController: NavHostController){
+    val context = LocalContext.current
+    Column {
+        Text(
+            text = "「台中市愛心家園」經市政府公開評選後，委託瑪利亞基金會經營管理，於91年啟用，整棟建築物有四個樓層，目前開辦就醫、就養、就學、就業四大領域的十項業務，提供身心障礙者全方位的服務。\n"
+        )
+        Text(
+            text = "長按以下圖片，可以觀看愛心家園地圖",
+            color = Color.Blue
+        )
+        Image(
+            painter = painterResource(id = R.drawable.lovehome),
+            contentDescription = "service",
+            modifier = Modifier.fillMaxSize()
+                .pointerInput(Unit) {
+                    detectTapGestures(
+                        onLongPress = {
+                            var it = Intent(Intent.ACTION_VIEW)
+                            it.data = Uri.parse("geo:0,0?q=台中市南屯區東興路一段450號")
+                            context.startActivity(it)
+                        },
+                    )
+                }
+        )
+    }
+}
+
+@Composable
+fun SecondScreen2(navController: NavHostController) {
+    val context = LocalContext.current
+    Column {
+        Text(
+            text = "「瑪利亞學園」提供重度以及極重度多重障礙者日間照顧服務，以健康照護為基礎，支持生活多面向參與及學習概念，輔助發展重度身心障礙者自我概念為最終服務目標。\n"
+        )
+        Text(
+            text = "雙擊以下圖片，可以觀看瑪利亞學園地圖",
+            color = Color.Blue
+        )
+        Image(
+            painter = painterResource(id = R.drawable.campus),
+            contentDescription = "service",
+            modifier = Modifier.fillMaxSize()
+                .pointerInput(Unit) {
+                    detectTapGestures(
+                        onDoubleTap = {
+                            var it = Intent(Intent.ACTION_VIEW)
+                            it.data = Uri.parse("geo:0,0?q=台中市北屯區經貿東路365號")
+                            context.startActivity(it)
+                        },
+                    )
+                }
+        )
     }
 }
 
@@ -106,6 +168,29 @@ fun Main2() {
                 )
                 Text(text = "主要機構",color = Color.Red)
 
+                Row {
+                    Button(onClick = {
+                        appear = false
+                        navController.navigate("JumpFirst")
+                    }) {
+                        Text(text = "台中市愛心家園")
+                    }
+                    Button(onClick = {
+                        appear = false
+                        navController.navigate("JumpSecond")
+                    }) {
+                        Text(text = "瑪利亞學園")
+                    }
+                }
+
+                NavHost(navController = navController, startDestination = "JumpFirst") {
+                    composable("JumpFirst") {
+                        FirstScreen1(navController = navController)
+                    }
+                    composable("JumpSecond") {
+                        SecondScreen2(navController = navController)
+                    }
+                }
             }
         }
     }
